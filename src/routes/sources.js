45c5f3c7e88274e600efefacff45a156476e8f6d
papageeleni <papageeleni@gapps.auth.gr>
2023-02-24 16:27:01 +0200

    added commentss

--- src/routes/sources.js
+++ src/routes/sources.js
@@ -7,12 +7,15 @@ const router = express.Router();
 
 const Source = require('../models/source');
 
+// This route retrieves all sources owned by the authenticated user
 router.get('/sources',
   authorization,
   async (req, res, next) => {
     try {
       const {id} = req.decoded;
+      // Find all sources owned by the user with the retrieved ID
       const foundSources = await Source.find({owner: mongoose.Types.ObjectId(id)});
+      // Map the retrieved sources into a new array containing only the necessary fields
       const sources = [];
       foundSources.forEach((s) => {
         sources.push({
@@ -36,12 +39,14 @@ router.get('/sources',
     }
   });
 
+// Route to create a new source
 router.post('/create-source', 
   authorization,
   async (req, res, next) => {
     try {
       const {name, type, url, login, passcode, vhost} = req.body;
       const {id} = req.decoded;
+      // Check if source with the same name already exists for the user
       const foundSource = await Source.findOne({owner: mongoose.Types.ObjectId(id), name});
       if (foundSource) {
         return res.json({
@@ -49,6 +54,7 @@ router.post('/create-source',
           message: 'A source with that name already exists.'
         });
       }
+      // Create new source and save to database
       await new Source({
         name,
         type,
@@ -65,19 +71,23 @@ router.post('/create-source',
     }
   }); 
 
+// Route to update an existing source 
 router.post('/change-source', 
   authorization,
   async (req, res, next) => {
     try {
+      // Extract source details, source id, and user id from request body and decoded token
       const {id, name, type, url, login, passcode, vhost} = req.body;
       const foundSource = await Source.findOne({_id: mongoose.Types.ObjectId(id), owner: mongoose.Types.ObjectId(req.decoded.id)});
+      // If the source is not found, return an error response
       if (!foundSource) {
         return res.json({
           status: 409,
           message: 'The selected source has not been found.'
         });
       }
-      
+
+      // Check if another source with the same name exists for the user 
       const sameNameSources = await Source.findOne({_id: {$ne: mongoose.Types.ObjectId(id)}, owner: mongoose.Types.ObjectId(req.decoded.id), name});
       if (sameNameSources) {
         return res.json({
@@ -86,6 +96,7 @@ router.post('/change-source',
         });
       }
 
+      // Update source details and save to database
       foundSource.name = name;
       foundSource.type = type;
       foundSource.url = url;
@@ -100,45 +111,54 @@ router.post('/change-source',
     }
   }); 
 
+// Route to delete an existing source 
 router.post('/delete-source', 
   authorization,
   async (req, res, next) => {
     try {
       const {id} = req.body;
-
+      // Find the source by ID and owner, and remove it from the database
       const foundSource = await Source.findOneAndRemove({_id: mongoose.Types.ObjectId(id), owner: mongoose.Types.ObjectId(req.decoded.id)});
       if (!foundSource) {
+        // If the source is not found, return an error response
         return res.json({
           status: 409,
           message: 'The selected source has not been found.'
         });
       }
+      // Return a success response
       return res.json({success: true});
     } catch (err) {
       return next(err.body);
     }
   }); 
 
+// Get information about a source
 router.post('/source',
   async (req, res, next) => {
     try {
       const {name, owner, user} = req.body;
+      // Determine the user ID to use for finding the source
       const userId = (owner === 'self') ? user.id : owner;
+      // Find the source by name and owner
       const foundSource = await Source.findOne({name, owner: mongoose.Types.ObjectId(userId)});
       if (!foundSource) {
+        // If the source is not found, return an error response
         return res.json({
           status: 409,
           message: 'The selected source has not been found.'
         });
       }
 
+      // Construct an object with the relevant source information to return
       const source = {};
       source.type = foundSource.type;
       source.url = foundSource.url;
       source.login = foundSource.login;
       source.passcode = foundSource.passcode;
       source.vhost = foundSource.vhost;
-    
+
+      // Return a success response with the source information
       return res.json({
         success: true,
         source
@@ -148,6 +168,7 @@ router.post('/source',
     }
   });
 
+// Get information about a source 
 router.post('/check-sources',
   authorization,
   async (req, res, next) => {
@@ -157,14 +178,17 @@ router.post('/check-sources',
 
       const newSources = [];
 
+      // loop through the sources array and check if each source exists in the database for the current user
       for (let i = 0; i < sources.length; i += 1) {
         // eslint-disable-next-line no-await-in-loop
         const result = await Source.findOne({name: sources[i], owner: mongoose.Types.ObjectId(id)});
         if (!result) {
+          // if the source doesn't exist, add it to the newSources array
           newSources.push(sources[i]);
         }
       }
 
+      // loop through the newSources array and create a new Source object for each new source
       for (let i = 0; i < newSources.length; i += 1) {
         // eslint-disable-next-line no-await-in-loop
         await new Source({
@@ -178,6 +202,7 @@ router.post('/check-sources',
         }).save();
       } 
       
+      // return a response with success and the newSources array
       return res.json({
         success: true,
         newSources
--- src/routes/users.js
+++ src/routes/users.js
@@ -9,47 +9,59 @@ const router = express.Router();
 const User = require('../models/user');
 const Reset = require('../models/reset');
 
+// Define a POST request handler for '/create' endpoint
 router.post('/create',
   (req, res, next) => validation(req, res, next, 'register'),
+  //Αsynchronous function to create a new user based on the request body
   async (req, res, next) => {
     const {username, password, email} = req.body;
     try {
+      // Check if a user with the given username or email already exists
       const user = await User.findOne({$or: [{username}, {email}]});
+      // If a user with the given username or email already exists, return an error response
       if (user) {
         return res.json({
           status: 409,
           message: 'Registration Error: A user with that e-mail or username already exists.'
         });
       }
+      // If a user with the given username or email does not exist, create a new user and save it to the database
       const newUser = await new User({
         username,
         password,
         email
       }).save();
+      // Return a success response with the ID of the newly created user
       return res.json({success: true, id: newUser._id});
     } catch (error) {
       return next(error);
     }
   });
 
+// Define a POST request handler for '/authenticate' endpoint
 router.post('/authenticate',
   (req, res, next) => validation(req, res, next, 'authenticate'),
+  //Αsynchronous function to authenticate a user based on the request body
   async (req, res, next) => {
     const {username, password} = req.body;
     try {
+      // Find a user with the given username and retrieve their hashed password from the database      
       const user = await User.findOne({username}).select('+password');
+      // If a user with the given username does not exist, return an error response
       if (!user) {
         return res.json({
           status: 401,
           message: 'Authentication Error: User not found.'
         });
       }
+      // Compare the hashed password from the database with the password provided in the request body      
       if (!user.comparePassword(password, user.password)) {
         return res.json({
           status: 401,
           message: 'Authentication Error: Password does not match!'
         });
       }
+      // If the password is correct, generate a JSON Web Token (JWT) and return it along with the user information
       return res.json({
         user: {
           username, 
@@ -63,27 +75,35 @@ router.post('/authenticate',
     }
   });
 
+// Define a POST request handler for '/resetpassword' endpoint
 router.post('/resetpassword',
   (req, res, next) => validation(req, res, next, 'request'),
+  // Define an asynchronous function to reset the password for a user based on the request body
   async (req, res, next) => {
     const {username} = req.body;
     try {
+      // Find a user with the given username in the database
       const user = await User.findOne({username});
+      // If a user with the given username does not exist, return an error response
       if (!user) {
         return res.json({
           status: 404,
           message: 'Resource Error: User not found.'
         });
       }
+      // Generate a JSON Web Token (JWT) containing the username
       const token = jwtSign({username});
+      // Remove any existing reset token for the user from the database, if present
       await Reset.findOneAndRemove({username});
+      // Save a new reset token for the user in the database
       await new Reset({
         username,
         token,
       }).save();
-
+      // Generate an email containing the reset token and send it to the user's email address
       const email = mail(token);
       send(user.email, 'Forgot Password', email);
+      // Return a success response indicating that the reset password email has been sent
       return res.json({
         ok: true,
         message: 'Forgot password e-mail sent.'
@@ -93,29 +113,38 @@ router.post('/resetpassword',
     }
   });
 
+// Define a POST request handler for '/changepassword' endpoint
 router.post('/changepassword',
+  // Call a function named 'validation' to validate the request body against the 'change' schema
   (req, res, next) => validation(req, res, next, 'change'),
   authorization,
+  // Define an asynchronous function to change the password for a user based on the request body
   async (req, res, next) => {
     const {password} = req.body;
     const {username} = req.decoded;
     try {
+      // Find a user with the given username in the database
       const user = await User.findOne({username});
+      // If a user with the given username does not exist, return an error response
       if (!user) {
         return res.json({
           status: 404,
           message: 'Resource Error: User not found.'
         });
       }
+      // Find and remove the reset token for the user from the database, if present
       const reset = await Reset.findOneAndRemove({username});
+      // If a reset token for the user does not exist, return an error response
       if (!reset) {
         return res.json({
           status: 410,
           message: ' Resource Error: Reset token has expired.'
         });
       }
+      // Update the user's password with the new password and save the changes to the database
       user.password = password;
       await user.save();
+      // Return a success response indicating that the password has been changed
       return res.json({
         ok: true,
         message: 'Password was changed.'
