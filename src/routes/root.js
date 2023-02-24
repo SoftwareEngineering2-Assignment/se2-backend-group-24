0acc4c51aa07043bbd172cee6fccc14ac3a52692
papageeleni <papageeleni@gapps.auth.gr>
2023-02-24 16:26:45 +0200

    added comments

--- src/routes/root.js
+++ src/routes/root.js
@@ -3,9 +3,10 @@ const path = require('path');
 
 const router = express.Router();
 
-const file = path.join(__dirname, '../../index.html');
+const file = path.join(__dirname, '../  ../index.html');
 router.use(express.static(file));
 
+// send as response to the root the root html file
 router.get('/', (req, res) => res.sendFile(file));
 
 module.exports = router;
