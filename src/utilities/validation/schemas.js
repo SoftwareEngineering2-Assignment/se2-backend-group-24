9faa3ebef54aad3efc71b92efeb237d23328ab2a
papageeleni <papageeleni@gapps.auth.gr>
2023-02-24 15:25:06 +0200

    added comments to the src\utilities\validation

--- src/utilities/validation/schemas.js
+++ src/utilities/validation/schemas.js
@@ -18,19 +18,23 @@ const password = yup
   .trim()
   .min(min);
 
+//make the username required
 const request = yup.object().shape({username: username.required()});
 
+// User authentication data is valid
 const authenticate = yup.object().shape({
-  username: username.required(),
-  password: password.required()
-});
+    username: username.required(),
+    password: password.required()
+  });
 
+// register required data
 const register = yup.object().shape({
   email: email.required(),
   password: password.required(),
   username: username.required()
 });
 
+// required data for update and checks of the parameters
 const update = yup.object().shape({
   username,
   password
@@ -39,6 +43,7 @@ const update = yup.object().shape({
   test: ({username: u, password: p}) => !(isNil(u) && isNil(p))
 });
 
+// makes password required for change
 const change = yup.object().shape({password: password.required()});
 
 module.exports = {
